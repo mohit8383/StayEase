1c72ad9c8e6e3e817214f685972a81392e70525c
Mohit <111625817+mohit8383@users.noreply.github.com>
2025-01-29 18:38:52 +0530

    settings.py

--- Django_practice_Pro_hotel_management_system-main/HotelManagementSystem/settings.py
+++ Django_practice_Pro_hotel_management_system-main/HotelManagementSystem/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dev&h09slzzbp!j(f^_lsen+afmt_&cnl96uus15mqnjc68)60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hotel-management-cqvn.onrender.com', '127.0.0.1', 'localhost']
 
